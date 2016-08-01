3b70765be0471a91e822e89ce52fdbbfde6ea6c5
imranfazley <imranfazley@gmail.com>
2016-08-01 16:17:05 +0800

    model User rspec testing passed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, :with => /@/
+  validates :password, presence: true, length: {in: 6..20}
+  validates :password_confirmation, presence: true
+  has_secure_password
+
+  has_many :statuses, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
 end
