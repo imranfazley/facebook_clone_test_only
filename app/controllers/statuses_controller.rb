7e07462a5c5a0c2015df47e891c6c6d619a07321
imranfazley <imranfazley@gmail.com>
2016-08-02 12:03:10 +0800

    rspec passing for status controller

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,18 +1,54 @@
 class StatusesController < ApplicationController
-
+before_action :set_params, only: [:update, :destroy]
   def new
+  	@status = Status.new
   end
 
   def create
+  	@current_user = User.find_by_email(params[:email])
+  	# session[:user_id] = @current_user.id
+
+  	if ((params[:status][:title]).length >= 8) && ((params[:status][:content]).length >= 10)
+  		# happy path
+  		# @status = Status.create(params[:status][:title],params[:status][:content])
+  		@status = Status.create(status_params)
+  		flash[:notice] = "Status is created successfully."
+  		redirect_to status_path(@status.id)
+  	else
+  		# unhappy path
+  		flash[:alert] = "Error creating status."
+  		render :new
+  	end
+
   end
 
   def update
+  	if @status.update(status_params)
+  		flash[:notice] = "Status is updated successfully."
+  		redirect_to status_path(@status.id)
+  	else
+  		render :edit
+  	end
+  		
   end
 
   def edit
   end
 
   def destroy
+  	@status.destroy
+  	flash[:notice] = "Status is deleted."
+  	redirect_to statuses_path
+  end
+
+  private
+
+  def status_params
+  	params.require(:status).permit(:title, :content)
+  end
+
+  def set_params
+  		@status = Status.find(params[:id])
   end
 
 end
